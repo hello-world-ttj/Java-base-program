0e5c4230b4588c621599bc4eb643c34d18dce7ec
tijotjoseph <tijotjoseph003@gmail.com>
2020-08-21 22:35:29 +0530

    Base program

---
+++ Base java programs/Concat.java
@@ -0,0 +1,13 @@
+class Concat {
+  public static void main(String[] args) {
+    // Concatenate "Hello" and "World", and print it
+    System.out.println("Hello"+"world");
+    
+    // Concatenate "38" and "19", and print it
+    System.out.println("38"+"19");
+    
+    // Add 38 and 19, and print it
+   System.out.println(38+19);
+    
+  }
+}
---
+++ Base java programs/Printing.java
@@ -0,0 +1,14 @@
+class Printing {
+  public static void main(String[] args) {
+    // Print 17 as an integer
+    System.out.println(17);
+    
+    // Print the sum of 5 and 3
+    System.out.println(5+3);
+    
+    System.out.println("5+3");
+    // Print "5 + 3" as a string
+    
+    
+  }
+}
