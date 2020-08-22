52368a0a867a94482192a0309f2a4ddd38d46de2
tijotjoseph <tijotjoseph003@gmail.com>
2020-08-22 15:05:24 +0530

    using mehod

---
+++ Base java programs/Using Methods/HelloW.java
@@ -0,0 +1,10 @@
+class HelloW {
+  public static void main(String[] args) {
+    hello();
+  }
+  
+  public static void hello() {
+    // Replace "Hello World" with "Hello Java"
+    System.out.println("Hello Java");
+  }
+}
