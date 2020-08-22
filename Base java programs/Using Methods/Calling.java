a1bd70ca61617881f7b68f6a193b8fd0e4ddd9ac
tijotjoseph <tijotjoseph003@gmail.com>
2020-08-22 15:32:13 +0530

    method programs

---
+++ Base java programs/Using Methods/Calling.java
@@ -0,0 +1,14 @@
+class Calling{
+  public static void main(String[] args) {
+    // Call the printData method
+    printData();
+    
+  }
+  
+  // Define the printData method
+  public static void printData()
+  {
+    System.out.println("My name is Kate Jones.");
+  }
+  
+}
